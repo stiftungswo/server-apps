331587d18d76e50fe62f9b63649ca85a96dd1eb7
Lukas Bischof <polinderis@gmail.com>
2019-07-22 17:39:09 +0200

    Documentation

--- traefik/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd /var/www/vhosts/stiftungswo.ch/traefik/
-docker-compose up -d
-
-find ./*/ -name "docker-compose.yml" \
--exec bash -c 'docker-compose -f {} \
---project-name $(basename $(dirname $(dirname {})))_$(basename $(dirname {})) \
-up -d --remove-orphans' \;
